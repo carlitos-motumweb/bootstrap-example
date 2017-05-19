d364da190fbf1d1ee0e042080e7c6ed1269515a2
Carlos Augusto <carlosflores@motumweb.com>
2017-05-19 15:01:43 +0000

    Se crea ModalPanel en catalogo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const $form = $('#suscribeFORM')
-const $formGroup  = $form.find('.form-group')
+const $formGroup = $form.find('.form-group')
 const $formControl = $formGroup.find('.form-control')
 
-$form.submit(function (ev){
+$form.submit(function(ev) {
     ev.preventDefault()
 
     $formGroup.removeClass().addClass('form-group')
@@ -10,19 +10,43 @@ $form.submit(function (ev){
     $formGroup.find('.form-control-feedback').remove();
 
     var newElement;
-    if(Math.random() > 0.5){
+    if (Math.random() > 0.5) {
         console.log('error')
         $formGroup.addClass('has-danger')
         $formControl.addClass('has-danger')
-        newElement = $('<div>',{html:'ha ocurrido un error'})
-    }else{
+        newElement = $('<div>', {
+            html: 'ha ocurrido un error'
+        })
+    } else {
         console.log('ok')
         $formGroup.addClass('has-success')
         $formControl.addClass('has-success')
-        newElement = $('<div>',{html:'Se te enviaran las novedades'})
+        newElement = $('<div>', {
+            html: 'Se te enviaran las novedades'
+        })
     }
     newElement.addClass('form-control-feedback')
     $formGroup.append(newElement)
 
 
 })
+
+function mostrarModal() {
+    console.log("no mostrar: " + localStorage.noMostrar)
+    if(localStorage.noMostrar == null){
+        noMostrarPanel = false
+    }else{
+        noMostrarPanel = JSON.parse(localStorage.noMostrar)
+    }
+
+    if (!noMostrarPanel) {
+        $('#modalOferta').modal()
+    }
+
+    $("#btnNOGracias").click(function(ev) {
+        console.log("se ha presionado el boton de NO-Gracias")
+        localStorage.noMostrar = true
+    })
+
+
+}
